e428c05b72efc0ee0f8ce1371bd9670d157716bd
Aditya Patharwat <95903545+WSBucky6032@users.noreply.github.com>
2023-06-16 17:08:04 +0530

    Created a new project 'Love Calculator Android App'

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
+function App() {
+  return (
+    <View>
+      <Text>hII</Text>
+    </View>
+  );
+}
+
+export default App;
